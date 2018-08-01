65d4c4d52865653c15ad153c8437709a632087cc
Matthias Petri <Matthias.Petri@gmail.com>
2018-08-01 17:11:01 +1000

    going home...

--- include/lm_cst_sent.hpp
+++ include/lm_cst_sent.hpp
@@ -23,8 +23,8 @@
 using cst_node_type = typename cst_type::node_type;
 
 struct instance_t {
-    bool all_one_hot;
     std::vector<uint32_t> sentence;
+    size_t one_hot_start = 0;
     size_t padding = 0;
     size_t real_len = 0;
     size_t rand = 0;
@@ -46,11 +46,16 @@ struct instance_t {
 };
 
 template <class t_itr>
-std::tuple<dynet::Expression, size_t> build_train_graph_cst_sent(
-    language_model& lm,
-    dynet::ComputationGraph& cg, t_itr& start, t_itr& end,
-    std::vector<std::vector<float>>& dists,const corpus_t& corpus,
-    double drop_out)
+std::tuple<dynet::Expression, size_t> 
+build_train_graph_prefix(language_model& lm,dynet::ComputationGraph& cg, t_itr& start, t_itr& end,double drop_out)
+{
+
+}
+
+
+template <class t_itr>
+std::tuple<dynet::Expression, size_t> 
+build_train_graph_regular(language_model& lm,dynet::ComputationGraph& cg, t_itr& start, t_itr& end,double drop_out)
 {
     size_t batch_size = std::distance(start, end);
     size_t sentence_len = start->sentence.size();
@@ -61,11 +66,10 @@ std::tuple<dynet::Expression, size_t> build_train_graph_cst_sent(
         lm.rnn.set_dropout(drop_out);
     }
 
-    i_R = dynet::parameter(cg, lm.p_R);
-    i_bias = dynet::parameter(cg, lm.p_bias);
-    // Initialize variables for batch errors
-    std::vector<dynet::Expression> errs;
+    lm.i_R = dynet::parameter(cg, lm.p_R);
+    lm.i_bias = dynet::parameter(cg, lm.p_bias);
 
+    std::vector<Expression> errs;
     // Set all inputs to the SOS symbol
     auto sos_tok = start->sentence.front();
     std::vector<uint32_t> current_tok(batch_size, sos_tok);
@@ -89,85 +93,15 @@ std::tuple<dynet::Expression, size_t> build_train_graph_cst_sent(
         // Run one step of the rnn : y_t = RNN(x_t)
         auto i_y_t = lm.rnn.add_input(i_x_t);
         // Project to the token space using an affine transform
-        auto i_r_t = i_bias + i_R * i_y_t;
-
+        auto i_r_t = lm.i_bias + lm.i_R * i_y_t;
         // Compute error for each member of the batch
-        dynet::Expression i_pred = -dynet::log_softmax(i_r_t);
-        dynet::Expression i_pred_linear = dynet::reshape(i_pred, { (unsigned int) dists[i].size() });
-        dynet::Expression i_true = dynet::input(cg, { (unsigned int)dists[i].size() }, dists[i]);
-        dynet::Expression i_error = dynet::transpose(i_true) * i_pred_linear;
-        errs.push_back(i_error);
-
+        auto i_err = dynet::pickneglogsoftmax(i_r_t, next_tok);
+        errs.push_back(i_err);
         // Change input
         current_tok = next_tok;
     }
     // Add all errors
-    return std::make_tuple(dynet::sum(errs), actual_predictions);
-}
-
-template<class t_itr>
-std::vector<std::vector<float>> compute_batch_losses(const cst_type& cst,const corpus_t& corpus,t_itr itr,t_itr end) {
-    size_t batch_size = std::distance(itr, end);
-    size_t sentence_len = itr->sentence.size();
-
-    static std::unordered_map<uint64_t,std::vector<float>> loss_cache;
-    
-    std::vector<std::vector<float>> losses(sentence_len);
-    for(size_t i=0;i<losses.size();i++) losses[i].resize(corpus.vocab.size()*batch_size);
-
-    for(size_t k=0;k<batch_size;k++) {
-        auto instance = *itr;
-        auto cur_node = cst.root();
-        for(size_t i=0;i<instance.sentence.size()-1;i++) {
-            auto& tok = instance.sentence[i];
-            // toks[i][k] = tok;
-            auto instance_loss_itr = losses[i].begin() + (corpus.vocab.size() * k);
-            size_t char_pos;
-            cur_node = cst.child(cur_node,tok,char_pos);
-            if(cst.is_leaf(cur_node)) {
-                // everything else is one hot
-                *(instance_loss_itr + instance.sentence[i+1]) = 1;
-                for(size_t j=i+1;j<instance.sentence.size()-1;j++) {
-                    // toks[j][k] = instance.sentence[j];
-                    instance_loss_itr = losses[j].begin() + (corpus.vocab.size() * k);
-                    *(instance_loss_itr + instance.sentence[j+1]) = 1;
-                }
-                break;
-            } else {
-                double node_size = cst.size(cur_node);
-                if(node_size > 25) {
-                    auto node_id = cst.id(cur_node);
-                    auto itr = loss_cache.find(node_id);
-                    if(itr != loss_cache.end()) {
-                        auto& stored_loss = itr->second;
-                        for(size_t i=0;i<stored_loss.size();i++) *(instance_loss_itr + i) = stored_loss[i];
-                    } else {
-                        std::vector<float> stored_loss(corpus.vocab.size(),0.0f);
-                        auto node_depth = cst.depth(cur_node);
-                        for (const auto& child : cst.children(cur_node)) {
-                            auto tok = cst.edge(child, node_depth + 1);
-                            double size = cst.size(child);
-                            *(instance_loss_itr + tok) = size / node_size;
-                            stored_loss[tok] = size / node_size;
-                        }
-                        loss_cache[node_id] = stored_loss;
-                    }
-                } else {
-                    auto node_depth = cst.depth(cur_node);
-                    std::vector<uint32_t> prefix(instance.sentence.begin(),instance.sentence.begin()+i+1);
-                    double node_size = cst.size(cur_node);
-                    for (const auto& child : cst.children(cur_node)) {
-                        auto tok = cst.edge(child, node_depth + 1);
-                        double size = cst.size(child);
-                        *(instance_loss_itr + tok) = size / node_size;
-                    }
-                }
-            }
-        }
-        ++itr;
-    }
-
-    return losses;
+    return std::make_tuple(sum_batches(sum(errs)), actual_predictions);
 }
 
 
@@ -180,6 +114,7 @@ void train_cst_sent(language_model& lm,const corpus_t& corpus, args_t& args)
     auto batch_size = args["batch_size"].as<size_t>();
     auto epoch_size = args["epoch_size"].as<size_t>();
     auto drop_out = args["drop_out"].as<double>();
+    int64_t report_interval = args["report_interval"].as<size_t>();
 
     CNLOG << "start training cst sentence lm";
     CNLOG << "\tepochs = " << num_epochs;
@@ -190,18 +125,29 @@ void train_cst_sent(language_model& lm,const corpus_t& corpus, args_t& args)
 
     // (1) create the batches
     CNLOG << "create the batches. batch_size = " << batch_size;
-    std::vector<instance_t> instances;
+    std::vector<instance_t> sentences;
     auto prep_start = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < corpus.num_sentences; i++) {
         auto start_sent = corpus.text.begin() + corpus.sent_starts[i];
         size_t sent_len = corpus.sent_lens[i];
-        instances.emplace_back(start_sent, sent_len);
+        sentences.emplace_back(start_sent, sent_len);
     }
     auto prep_end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> prep_diff = prep_end - prep_start;
     CNLOG << "created batches in " << " - " << prep_diff.count() << "s";
 
-    CNLOG << "NUMBER OF INSTANCES = " << instances.size();
+    CNLOG << "create the prefix batches. batch_size = " << batch_size;
+    auto prefix_start = std::chrono::high_resolution_clock::now();
+    auto lb = cst.csa.C[corpus.vocab.start_sent_tok];
+    auto rb = cst.csa.C[corpus.vocab.start_sent_tok + 1] - 1;
+    auto start_node = cst.node(lb,rb); // cst node of <s>
+    prefixes = find_all_prefixes(cst,corpus,start_node);
+    auto prefix_end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> prefix_diff = prefix_end - prefix_start;
+    CNLOG << "created prefix batches in " << " - " << prefix_diff.count() << "s";
+
+    CNLOG << "number of sentences = " << sentences.size();
+    CNLOG << "number of prefixes = " << prefixes.size();
     dynet::AdamTrainer trainer(lm.model, 0.001, 0.9, 0.999, 1e-8);
     trainer.clip_threshold = trainer.clip_threshold * batch_size;
     std::mt19937 gen(constants::RAND_SEED);
