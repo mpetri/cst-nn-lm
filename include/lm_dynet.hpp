c7c73faa882f64b854a502979f38fed283168448
Matthias Petri <Matthias.Petri@gmail.com>
2018-08-06 15:23:00 +1000

    welcome back clipping

--- include/lm_dynet.hpp
+++ include/lm_dynet.hpp
@@ -129,6 +129,7 @@ void train_dynet_lm(language_model& lm,const corpus_t& corpus, args_t& args,t_tr
     CNLOG << "created batches in " << " - " << prep_diff.count() << "s";
 
     CNLOG << "number of sentences = " << sentences.size();
+    trainer.clip_threshold = trainer.clip_threshold * batch_size;
     std::mt19937 gen(constants::RAND_SEED);
     std::uniform_int_distribution<> dis(0,100000000);
     double best_pplx = std::numeric_limits<double>::max();
